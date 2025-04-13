c0dd33da837de5c43f7411b51e3389d32163ddef
xrueiii <b11705043@g.ntu.edu.tw>
2025-04-14 00:55:09 +0800

    Modified ESLint

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,14 +1,15 @@
-export default [
+// eslint.config.cjs
+module.exports = [
   {
     files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: 'script'
+      sourceType: 'script',
     },
     rules: {
       'no-unused-vars': 'warn',
       'no-undef': 'error',
-      'semi': ['warn', 'always']
-    }
+      'semi': ['warn', 'always'],
+    },
   }
 ];
