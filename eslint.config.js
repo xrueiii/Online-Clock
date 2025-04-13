6edb165cf2e5d7a76a77b72bc08b59d4a3b2b3e2
xrueiii <b11705043@g.ntu.edu.tw>
2025-04-14 00:52:50 +0800

    Fixed ESLint version

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'script'
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'semi': ['warn', 'always']
+    }
+  }
+];
